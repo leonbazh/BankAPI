0da7fd50b636ce8216dbcefea68b927c6e469139
kristieg <earsy@earsy>
2024-11-03 19:44:35 +0300

    added environment file

--- init.py
+++ init.py
@@ -1,19 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/postgres'
-    SECRET_KEY = 'secret_key'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
+
+from operations import * 
+
 with app.app_context():
     db.create_all()
-
-from operations import *
 
 if __name__ == '__main__':
     app.run(debug=True)
